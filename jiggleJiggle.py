ae72c70533b13ff381945ac0be9c33d5bb4c1f85
SirZombieFlambe <zakerynsnider@gmail.com>
2023-10-19 12:14:45 -0600

    Update

--- jiggleJiggle.py
+++ jiggleJiggle.py
@@ -5,11 +5,9 @@ import bot
 # from discord.ext import commands
 
 
-
 TOKEN_FILE_1 = "TOKEN_1.txt"  # Replace with your file path
 with open(TOKEN_FILE_1, "r") as file:
     TOKEN_1 = file.readlines()
 file.close()
 
 bot.run_discord_bot(TOKEN_1[0])
-
