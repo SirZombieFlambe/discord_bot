db3bd555c7b1e82e927ce913850a67da15ec0eb5
SirZombieFlambe <zakerynsnider@gmail.com>
2024-05-29 23:31:59 -0600

    Kinda a Test

--- big_brother.py
+++ big_brother.py
@@ -1,4 +1,5 @@
 '''
-This is for Server Viewing
+This is for Server Viewing - Should View different aspects of
+server life ie things like leaving and joining vc
 Make sure implement every feature with a settings aspect too
 '''
